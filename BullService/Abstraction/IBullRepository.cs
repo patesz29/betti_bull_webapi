e6a62ec86a468b594b981705047032c73fd0e034
Patesz <Patesz@PATESZ-DESKTOP>
2019-11-09 15:35:45 +0100

    Initial project

---
+++ BullService/Abstraction/IBullRepository.cs
@@ -0,0 +1,13 @@
+﻿using BullService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BullService.Abstraction
+{
+    public interface IBullRepository
+    {
+        Task<IOperationResult<IEnumerable<BaseModel>>> GetBulls();
+    }
+}
---
+++ BullService/Repository/BullRepository.cs
@@ -0,0 +1,17 @@
+﻿using BullService.Abstraction;
+using BullService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BullService.Repository
+{
+    public class BullRepository : IBullRepository
+    {
+        public Task<IEnumerable<BaseModel>> GetBulls()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
